2968659b854c5b1a85f00554279f50d25ec52f1e
sambeau <sambeau@mac.com>
2026-02-01 12:47:40 +0000

    Update test entity to Entity.js

--- bits/ball.js
+++ bits/ball.js
@@ -1,45 +1,58 @@
+import { canvas, ctx } from "../game/game.js"
+import { pick, randInt } from "/zap/zap.js"
+
+import { createEntity } from "../game/entities/Entity.js"
+
 const ball = () => {
 	return {
-		x: 10,
-		y: 100,
-		vx: 20,
-		vy: 10,
+		...createEntity( {
+			name: "ball",
+			width: 25,
+			height: 25,
+			collider: { type: "circle", ox: 0, oy: 0, r: 25, colliding: false }
+		} ),
+
 		radius: 25,
 		color: "white",
-		spawn() {
-			this.color = pick(["#fff", "#0ff", "#ff0", "#f0f", "#0ff"])
-			this.x = randInt(canvas.width)
-			this.y = randInt(canvas.height)
-			this.radius = randInt((randInt(10) + 1))
-			this.vx = randInt(20) + 1
-			this.vy = randInt(20) + 1
-			this.width = (1 * this.radius)
-			this.height = (1 * this.radius)
+		vx: 20,
+		vy: 10,
+
+		spawn () {
+			this.color = pick( [ "#fff", "#0ff", "#ff0", "#f0f", "#0ff" ] )
+			this.x = randInt( canvas.width )
+			this.y = randInt( canvas.height )
+			this.radius = randInt( randInt( 10 ) + 1 )
+			this.vx = randInt( 20 ) + 1
+			this.vy = randInt( 20 ) + 1
+			this.width = this.radius
+			this.height = this.radius
+			this.collider.r = this.radius
 		},
-		outOfBoundsV() {
-			if (this.y > canvas.height - this.height || this.y < (0 + this.height)) return true
-			return false;
-		},
-		outOfBoundsH() {
-			if (this.x > canvas.width - this.width || this.x < (0 + this.width)) return true
-			return false
-		},
-		update(dt) {
-			this.x += this.vx;
-			this.y += this.vy;
-			if (this.outOfBoundsH()) {
-				this.vx = 0 - this.vx;
+
+		update () {
+			this.tick()
+			this.x += this.vx
+			this.y += this.vy
+
+			// Bounce off edges
+			if ( this.x > canvas.width - this.width || this.x < this.width ) {
+				this.vx = -this.vx
 			}
-			if (this.outOfBoundsV()) {
-				this.vy = 0 - this.vy;
+			if ( this.y > canvas.height - this.height || this.y < this.height ) {
+				this.vy = -this.vy
 			}
+
+			this.syncCollider()
 		},
-		draw() {
-			ctx.beginPath();
-			ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
-			ctx.closePath();
-			ctx.fillStyle = this.color;
-			ctx.fill();
+
+		draw () {
+			ctx.beginPath()
+			ctx.arc( this.x, this.y, this.radius, 0, Math.PI * 2, true )
+			ctx.closePath()
+			ctx.fillStyle = this.color
+			ctx.fill()
 		},
 	}
-};
+}
+
+export { ball }
